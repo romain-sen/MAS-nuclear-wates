eac33b39ece743771c30596f3968bca12d2efa35
raphael_mgy <raphael.menguy@pricehubble.com>
2024-04-12 13:52:20 +0200

    experimental analysis on simulation

--- without-communication/run.py
+++ without-communication/run.py
@@ -8,7 +8,11 @@ model = NuclearWasteModel(
     n_red_agents=10,
     n_wastes=10,
     max_wastes_handed=5,
+<<<<<<< HEAD
     strategy=1,
 )
+=======
+    )
+>>>>>>> 2c4751d (experimental analysis on simulation)
 for i in range(10):
     model.step()
