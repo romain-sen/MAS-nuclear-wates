49bc710cb5ef6b8ecd9410a06565c8e4a2ad8a55
Martin Douysset <martin.douysset@student-cs.fr>
2024-04-12 16:21:51 +0200

    upper_agent_v3

--- without-communication/agent_strat_1.py
+++ without-communication/agent_strat_1.py
@@ -32,11 +32,11 @@ class DefaultAgent(CleaningAgent):
         action = movables[self.random.randrange(len(movables))]
         # If the agent is on a waste, not on the top row and has a free spot, take it
         if (
-            (last_percept["wastes"] is not None and len(last_percept["wastes"]) < 2)
+            (last_percept["wastes"] is not None and len(
+                last_percept["wastes"]) < 2)
             and self.model.is_on_waste(self.pos) is not None
             and last_percept["pos"][1] < self.knowledge["grid_height"] - 1
         ):
-            # TODO : Check if the agent is on top without using the height of the grid
             action = Action.TAKE
 
         if len(last_percept["wastes"]) == 2:
@@ -79,10 +79,10 @@ class DefaultAgent(CleaningAgent):
                             else:
                                 # Otherwise, go somewhere else to drop it
                                 movables = [Action.LEFT, Action.RIGHT]
-                                action = movables[self.random.randrange(len(movables))]
+                                action = movables[self.random.randrange(
+                                    len(movables))]
             elif len(self.knowledge["percepts"]) == 1:
                 action = Action.UP
-
         return action
 
 
@@ -106,18 +106,24 @@ class UpperLineAgent(CleaningAgent):
                     movables = [Action.LEFT, Action.RIGHT]
                     action = movables[self.random.randrange(len(movables))]
 
-                # If the agent has only one waste, go right if you can
-                if len(last_percept["wastes"]) == 1:
-                    if self.pos[0] != self.knowledge["x_max"] - 1:
-                        action = Action.RIGHT
+                # If the agent has only one red waste, go right if you can
+                if (len(last_percept["wastes"]) == 1
+                    and self.pos[0] != self.knowledge["x_max"]
+                        and last_percept["wastes"][0].indicate_color() == AgentColor.RED):
+                    action = Action.RIGHT
 
                 # if  the agent cannot move rigth, drop the waste if red waste
                 if (
-                    self.pos[0] == self.knowledge["x_max"] - 1
+                    self.pos[0] == self.knowledge["x_max"]
                     and len(last_percept["wastes"]) == 1
                     and last_percept["wastes"][0].indicate_color() == AgentColor.RED
                 ):
                     action = Action.DROP
+
+                # If the agent has a non red waste, move left or right
+                if len(last_percept["wastes"]) == 1 and last_percept["wastes"][0].indicate_color() != AgentColor.RED:
+                    movables = [Action.LEFT, Action.RIGHT]
+                    action = movables[self.random.randrange(len(movables))]
 
                 # If the agent has a waste, and is on a waste of the same color, take it if the waste is not red
                 if (
@@ -133,7 +139,7 @@ class UpperLineAgent(CleaningAgent):
                 if (
                     self.model.is_on_waste(self.pos) is not None
                     and len(last_percept["wastes"]) == 0
-                    and self.pos[0] != self.knowledge["x_max"] - 1
+                    and self.pos[0] != self.knowledge["x_max"]
                 ):
                     action = Action.TAKE
 
@@ -208,5 +214,6 @@ def add_agents_strat_1(environment, n_green_agents, n_yellow_agents, n_red_agent
     add_cleaning_agents(environment, n_green_agents, AgentColor.GREEN)
     add_cleaning_agents(environment, n_yellow_agents, AgentColor.YELLOW)
     red_upper_agents = n_red_agents // 2
-    add_cleaning_agents(environment, n_red_agents - red_upper_agents, AgentColor.RED)
+    add_cleaning_agents(environment, n_red_agents -
+                        red_upper_agents, AgentColor.RED)
     add_upper_agents(environment, red_upper_agents)
